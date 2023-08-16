5cfe893b1e85521efdace27a649f21417140aac7
K Manoranjan <34857983+manoranjan99@users.noreply.github.com>
2023-08-16 19:36:23 +0530

    project file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}",
+],
+  theme: {
+    extend: {},
+    theme: {
+      fontFamily: {
+        //Opensans: ['Open Sans', 'sans-serif'],
+        Poppins:['Poppins', 'sans-serif']
+      },
+    },
+  },
+  plugins: [
+    
+  ],
+
+}
